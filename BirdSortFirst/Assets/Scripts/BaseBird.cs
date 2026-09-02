be7eff125476adf633c521b8fbba5018ea27a3bc
DoiThanhDat <datdt1.gplaystudio@gmail.com>
2026-09-02 17:37:50 +0700

    Lam anim cho chim (dang lam do lan 1)

--- BirdSortFirst/Assets/Scripts/BaseBird.cs
+++ BirdSortFirst/Assets/Scripts/BaseBird.cs
@@ -1,20 +1,70 @@
 using UnityEngine;
 using DG.Tweening;
+using Spine.Unity;
 
 public class BaseBird : MonoBehaviour
 {
     public BaseBranch currentBranch;
     public int ID;
     public float moveSpeed;
+    public const string FLY = "fly";
+    public const string GROUNDING = "ground";
+    public const string IDLE = "ilde";
+
+    [SerializeField] protected SkeletonGraphic body;
+    public SkeletonGraphic Body => body;
+
+    private void Start()
+    {
+        PlayIdle();
+    }
+    #region Idle, Fly && Grounding
+    public void PlayIdle()
+    {
+        if (body != null && !string.IsNullOrEmpty(IDLE))
+        {
+            body.AnimationState.SetAnimation(0, IDLE, true);
+        }
+    }
+
+    public void PlayFly()
+    {
+        if (body != null && !string.IsNullOrEmpty(FLY))
+        {
+            body.AnimationState.SetAnimation(0, FLY, true);
+        }
+    }
+
+    public void PlayGrounding()
+    {
+        if (body != null && !string.IsNullOrEmpty(FLY))
+        {
+            body.AnimationState.AddAnimation(0, GROUNDING, false, 0f);
+        }
+        else
+        {
+            PlayIdle();
+        }
+    }
+    #endregion
+
+    #region Move To
     public void MoveTo(Vector3 targetPosition, System.Action onMoveCompleted = null)
     {
+        transform.DOKill();
+        if (body != null && body.Skeleton != null)
+        {
+            body.Skeleton.ScaleX = (targetPosition.x < transform.position.x) ? -1f : 1f;
+        }    
+        PlayFly();
         float distance = Vector3.Distance(transform.position, targetPosition);
         float moveDuration = distance / moveSpeed;
         transform.DOMove(targetPosition, moveDuration).OnComplete(() =>
         {
+            PlayGrounding();
             onMoveCompleted?.Invoke();
         });
     }
-    
+    #endregion
 
 }
